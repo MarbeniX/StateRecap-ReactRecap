abbdb71749db30504f23f0cd54e221a26de98d5e
Berlin Ortega <marlitros200002@gmail.com>
2025-07-23 14:41:26 -0600

    App

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./index.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const messages = [
+    "Welcome to the first step!",
+    "You are now on the second step.",
+    "Congratulations, you have completed all steps!",
+];
+
+export default function App() {
+    const [step, setStep] = useState(1);
+    const [open, setOpen] = useState(true);
+
+    return (
+        <>
+            <button className="close" onClick={() => setOpen((is) => !is)}>
+                x
+            </button>
+            {open && (
+                <div className="steps">
+                    <div className="numbers">
+                        <div className={`${step >= 1 ? "active" : ""}`}>1</div>
+                        <div className={`${step >= 2 ? "active" : ""}`}>2</div>
+                        <div className={`${step >= 3 ? "active" : ""}`}>3</div>
+                    </div>
+
+                    <p className="message">
+                        Step {step}: {messages[step - 1]}
+                    </p>
+
+                    <div className="buttons">
+                        <button
+                            style={{ background: "#7950f2", color: "#fff" }}
+                            onClick={() =>
+                                setStep(step > 1 ? (s) => s - 1 : (s) => s)
+                            }
+                        >
+                            Previous
+                        </button>
+                        <button
+                            style={{ background: "#7950f2", color: "#fff" }}
+                            onClick={() =>
+                                setStep(step < 3 ? (s) => s + 1 : (s) => s)
+                            }
+                        >
+                            Next
+                        </button>
+                    </div>
+                </div>
+            )}
+        </>
+    );
 }
-
-export default App;
